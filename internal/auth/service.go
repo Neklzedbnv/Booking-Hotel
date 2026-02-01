8240eb183f2898c59e7b7858abd30b04a2431f6d
Abzal <166910313+Neklzedbnv@users.noreply.github.com>
2026-02-01 17:47:13 +0500

    Nurasyl

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -1,1 +1,14 @@
 package auth
+
+type Service struct {
+	repo *Repo
+}
+
+func NewService(repo *Repo) *Service {
+	return &Service{repo: repo}
+}
+
+// Stub method (authentication not required yet)
+func (s *Service) Ping() bool {
+	return true
+}
