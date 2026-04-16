cb7e012ee9da7b2aed39032bfafd808223c27b91
nuratop40@gmail.com <abzal.bakhtiyarov@mail.ru>
2026-04-16 15:02:04 +0500

    fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,62 +9,52 @@ import (
 	introuter "Gofinal/internal/http"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
-	
+
 	dbConn := db.NewPostgres()
 
-	
 	db.RunMigrations(dbConn)
 
-	
 	authRepo := auth.NewRepo(dbConn)
 	authService := auth.NewService(authRepo)
 	authHandler := auth.NewHandler(authService)
 
-	
 	roomRepo := catalog.NewRoomRepo(dbConn)
 	roomService := catalog.NewRoomService(roomRepo)
 	roomHandler := catalog.NewRoomHandler(roomService)
 
-	
 	packageRepo := catalog.NewPackageRepo(dbConn)
 	packageService := catalog.NewPackageService(packageRepo)
 	packageHandler := catalog.NewPackageHandler(packageService)
 
-	
 	svcRepo := catalog.NewSvcRepo(dbConn)
 	svcService := catalog.NewSvcService(svcRepo)
 	svcHandler := catalog.NewSvcHandler(svcService)
 
-	
 	mpRepo := catalog.NewMealPlanRepo(dbConn)
 	mpService := catalog.NewMealPlanService(mpRepo)
 	mpHandler := catalog.NewMealPlanHandler(mpService)
 
-	
 	paymentRepo := booking.NewPaymentRepo(dbConn)
 	paymentService := booking.NewPaymentService(paymentRepo)
 	paymentHandler := booking.NewPaymentHandler(paymentService)
 
-	
 	bookingRepo := booking.NewRepo(dbConn)
 	bookingService := booking.NewService(bookingRepo, roomRepo, mpRepo, packageRepo, paymentRepo)
 	bookingHandler := booking.NewHandler(bookingService)
 
-	
 	reviewRepo := booking.NewReviewRepo(dbConn)
 	reviewService := booking.NewReviewService(reviewRepo)
 	reviewHandler := booking.NewReviewHandler(reviewService)
 
-	
 	adminRepo := admin.NewRepo(dbConn)
 	adminService := admin.NewService(adminRepo)
 	adminHandler := admin.NewHandler(adminService)
 
-	
 	pageHandler := introuter.NewPageHandler(
 		"UI",
 		roomService,
@@ -75,7 +65,6 @@ func main() {
 		reviewService,
 	)
 
-	
 	router := introuter.NewRouter(
 		authHandler,
 		bookingHandler,
@@ -90,7 +79,6 @@ func main() {
 	)
 	handler := router.SetupRoutes()
 
-	
 	go func() {
 		for {
 			log.Println("background worker alive")
@@ -98,7 +86,11 @@ func main() {
 		}
 	}()
 
-	// ── Start server ────────────────────────────────────────────
-	log.Println("Server started on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", handler))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("Server started on port " + port)
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
